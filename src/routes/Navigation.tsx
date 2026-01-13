815cbb29916d3fbf371d585831f98b5f1505917a
Fercasanov <fernando.casanova1992@outlook.com>
2026-01-13 16:08:19 -0300

    fix : se actualiza a react router dom v6

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,8 +1,9 @@
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
-  NavLink
+  NavLink,
+  Navigate
 } from 'react-router-dom';
 
 import logo from '../logo.svg';
@@ -15,30 +16,25 @@ export const Navigation = () => {
             <img src={ logo } alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink to="/" className={({ isActive }) => isActive ? 'nav-active' : ''}>Home</NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+              <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink>
             </li>
             <li>
-              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+              <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : ''}>Users</NavLink>
             </li>
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+        {/* A <Routes> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <h1>About</h1>
-          </Route>
-          <Route path="/users">
-            <h1>Users</h1>
-          </Route>
-          <Route path="/">
-            <h1>Home</h1>
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/users" element={<h1>Users</h1>} />
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/*" element={<Navigate to="/home" replace/>} />
+        </Routes>
       </div>
     </Router>
   );
